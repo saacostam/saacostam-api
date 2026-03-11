173cdba806f0bf33155923e45dc807e993787a17
saacostam <73088662+saacostam@users.noreply.github.com>
2026-03-10 21:15:21 -0500

    style(format)

--- src/analytics/test/mock-context.ts
+++ src/analytics/test/mock-context.ts
@@ -2,29 +2,29 @@ import { vi } from "vitest";
 import type { IContext } from "../app";
 
 export function mockContext() {
-    // Providers
-    const dateProviderNow = vi.fn();
-    
-    // Repositories
-    const analyticsEventsRepositoryCreate = vi.fn();
-    const analyticsEventsRepositoryGetAll = vi.fn();
+	// Providers
+	const dateProviderNow = vi.fn();
 
-    const ctx: IContext = {
-        dateProvider: {
-            now: dateProviderNow,
-        },
-        analyticsEventsRepository: {
-            create: analyticsEventsRepositoryCreate,
-            getAll: analyticsEventsRepositoryGetAll,
-        }
-    }
+	// Repositories
+	const analyticsEventsRepositoryCreate = vi.fn();
+	const analyticsEventsRepositoryGetAll = vi.fn();
 
-    return {
-        ctx,
-        // Providers
-        dateProviderNow,
-        // Repositories
-        analyticsEventsRepositoryCreate,
-        analyticsEventsRepositoryGetAll,
-    }
+	const ctx: IContext = {
+		dateProvider: {
+			now: dateProviderNow,
+		},
+		analyticsEventsRepository: {
+			create: analyticsEventsRepositoryCreate,
+			getAll: analyticsEventsRepositoryGetAll,
+		},
+	};
+
+	return {
+		ctx,
+		// Providers
+		dateProviderNow,
+		// Repositories
+		analyticsEventsRepositoryCreate,
+		analyticsEventsRepositoryGetAll,
+	};
 }
--- src/analytics/test/mock-events.ts
+++ src/analytics/test/mock-events.ts
@@ -1,14 +1,14 @@
 import type { IEvent } from "../domain";
 
 export const MOCK_EVENTS: IEvent[] = [
-    {
-        name: "test-name-1",
-        payload: "test-payload-1",
-        createdAt: new Date(200),
-    },
-    {
-        name: "test-name-2",
-        payload: "test-payload-2",
-        createdAt: new Date(200),
-    },
-]
+	{
+		name: "test-name-1",
+		payload: "test-payload-1",
+		createdAt: new Date(200),
+	},
+	{
+		name: "test-name-2",
+		payload: "test-payload-2",
+		createdAt: new Date(200),
+	},
+];
