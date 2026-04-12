c8345354078f8e6ae59006b884a6a839149713be
Santiago Acosta Meza <73088662+saacostam@users.noreply.github.com>
2026-04-12 14:13:26 -0500

    feat(ttt): write ws event adapter

--- src/apps/tic-tac-toe/shared/adapters/infra/index.ts
+++ src/apps/tic-tac-toe/shared/adapters/infra/index.ts
@@ -1,1 +1,2 @@
 export * from "./uuid-adapter-impl";
+export * from "./ws-event-adapter";
---
+++ src/apps/tic-tac-toe/shared/adapters/infra/ws-event-adapter.ts
@@ -0,0 +1,39 @@
+import type { WebSocket } from "ws"
+import type { IEventAdapter, IEventType } from "@/apps/tic-tac-toe/shared/adapters/domain";
+
+export class WsEventAdapter implements IEventAdapter {
+    private conns: Map<string, WebSocket> = new Map();
+
+    addConnection(userId: string, conn: WebSocket) {
+        return this.conns.set(userId, conn);
+    }
+
+    removeConnection(userId: string) {
+        if (this.conns.has(userId)) {
+            this.conns.delete(userId);
+        }
+    }
+
+    async broadcast(event: IEventType, message?: string): Promise<void> {
+        for (const [_, conn] of this.conns) {
+            this._send(conn, event, message);
+        }
+    }
+
+    async publish(id: string, event: IEventType, message?: string): Promise<void> {
+        const conn = this.conns.get(id);
+
+        if (!conn) return;
+
+        this._send(conn, event, message);
+    }
+
+    async _send(conn: WebSocket, event: IEventType, message?: string): Promise<void> {
+        const payload = {
+            event,
+            message: message,
+        };
+
+        conn.send(JSON.stringify(payload));
+    }
+}
