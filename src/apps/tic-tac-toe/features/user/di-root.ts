3bdba468ece9180572026299eef17792f48c0284
saacostam <73088662+saacostam@users.noreply.github.com>
2026-04-12 22:11:51 -0500

    feat(ttt:di): export user repository

--- src/apps/tic-tac-toe/features/user/di-root.ts
+++ src/apps/tic-tac-toe/features/user/di-root.ts
@@ -2,6 +2,6 @@ import { uuidAdapter } from "@/apps/tic-tac-toe/shared/adapters/di-root";
 import { UserUseCases } from "./app";
 import { InMemoryUserRepository } from "./infra";
 
-const userRepository = new InMemoryUserRepository(uuidAdapter);
+export const userRepository = new InMemoryUserRepository(uuidAdapter);
 
 export const userUseCases = new UserUseCases(userRepository);
