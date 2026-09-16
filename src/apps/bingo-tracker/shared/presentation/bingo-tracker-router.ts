8c55afa9fa919108482e4043451228d17b1d77a4
saacostam <73088662+saacostam@users.noreply.github.com>
2026-09-15 22:48:34 -0500

    feat(bingo): add game to app router

--- src/apps/bingo-tracker/shared/presentation/bingo-tracker-router.ts
+++ src/apps/bingo-tracker/shared/presentation/bingo-tracker-router.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authRouter } from "@/apps/bingo-tracker/features/auth/presentation";
+import { gameRouter } from "@/apps/bingo-tracker/features/game/presentation";
 import { userRouter } from "@/apps/bingo-tracker/features/user/presentation";
 import { errorHandlerMiddleware } from "@/shared/errors/presentation";
 
 export const bingoTrackerRouter = Router();
 
 bingoTrackerRouter.use("/auth", authRouter);
+bingoTrackerRouter.use("/game", gameRouter);
 bingoTrackerRouter.use("/user", userRouter);
 
 bingoTrackerRouter.use(errorHandlerMiddleware);
