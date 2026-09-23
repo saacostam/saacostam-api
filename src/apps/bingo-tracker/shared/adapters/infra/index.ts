aeab96802ce22ee1bfc209c36394d881b09e8401
saacostam <73088662+saacostam@users.noreply.github.com>
2026-09-22 20:43:34 -0500

    feat(bingo): implement mock vision provider

--- src/apps/bingo-tracker/shared/adapters/infra/index.ts
+++ src/apps/bingo-tracker/shared/adapters/infra/index.ts
@@ -1,5 +1,6 @@
 export * from "./bcrypt-password-hasher";
 export * from "./jwt-token-adapter";
 export * from "./mock-error-logger";
+export * from "./mock-vision-provider";
 export * from "./uuid-generator";
 export * from "./vanilla-date-adapter";
---
+++ src/apps/bingo-tracker/shared/adapters/infra/mock-vision-provider.ts
@@ -0,0 +1,23 @@
+import type {
+	VisionProvider,
+	VisionProviderPayload,
+} from "@/apps/bingo-tracker/shared/adapters/domain";
+
+export class MockVisionProvider implements VisionProvider {
+	async extractBoard({
+		image: _,
+		description: __,
+	}: VisionProviderPayload["extractBoard"]["req"]): Promise<
+		VisionProviderPayload["extractBoard"]["res"]
+	> {
+		return {
+			board: [
+				[1, 2, 3, 4, 5],
+				[6, 7, 8, 9, 10],
+				[11, 12, 13, 14, 15],
+				[16, 17, 18, 19, 20],
+				[21, 22, 23, 24, 25],
+			],
+		};
+	}
+}
